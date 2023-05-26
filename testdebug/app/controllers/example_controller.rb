43b620e0e885aff99b2fe2432d0154270cacccb5
Raskolnikov <jerry.tann@endress.com>
2023-05-26 10:02:52 +0800

    🚀 try newer rails and ruby

--- testdebug/app/controllers/example_controller.rb
+++ testdebug/app/controllers/example_controller.rb
@@ -2,6 +2,10 @@ class ExampleController < ApplicationController
   def show
     sum = 1+1
     puts "sum is #{sum}"
+    debugger
+    sum += 5
+    debugger
     render json: { message: 'Hello, world!' }
+
   end
 end
